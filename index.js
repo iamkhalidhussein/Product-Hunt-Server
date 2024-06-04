2a81705afbd83932e3bc18e30f4dae5b9b2d47a7
khalid09dev <mdkhalidhossen@hotmail.com>
2024-06-04 12:13:49 +0600

    added codes for getting data from db for featured products section

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const featuredProductsCollection = client.db("featuredProducts").collection("Products");
+
+        app.get('/featured-products', async(req, res) => {
+            const result = await featuredProductsCollection.find().toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
