ffdc78df3f54c24fe86d11fe8cc05a9c2e42c7ed
khalid09dev <mdkhalidhossen@hotmail.com>
2025-01-23 22:18:43 +0600

    refactored userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,28 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 
-router.get('/', verifyToken, verifyAdmin, userController.getUsers);
+//authentication routes
+router.post('/jwt', userController.jwtController);
+
+//user profile routes
 router.get('/userprofileinfo/:email', verifyToken, userController.getUserProfileInfo)
 router.post('/userprofileinfo/:email', verifyToken, userController.postUserProfileInfo)
 router.patch('/updateuserprofileinfo/:email', userController.updateUserProfileInfo)
+
+//admin routes
+router.get('/admin/:email', verifyToken, userController.getAdminStatus);
+
+//moderator routes
+router.get('/moderator/:email', verifyToken,userController.getModeratorStatus);
+
+//payment info routes
+router.get('/paymentinfo/:email', userController.getPaymentInfo);
+
+//upvote routes
 router.patch('/:useremail/:cartId', userController.doUpvote);
 router.patch('/userrs/:useremail/:cartId', userController.doUpvoteFeatured);
-router.get('/paymentinfo/:email', userController.getPaymentInfo);
-router.get('/admin/:email', verifyToken, userController.getAdminStatus);
-router.get('/moderator/:email', verifyToken,userController.getModeratorStatus);
-router.post('/jwt', userController.jwtController);
+
+//general user routes (admin-only)
+router.get('/', verifyToken, verifyAdmin, userController.getUsers);
 
 module.exports = router;
